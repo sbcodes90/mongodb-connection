8c58884c323b8b6174787bdcc5eb82d201dce536
sbcodes90 <sydneycburrell29@gmail.com>
2024-01-25 15:35:47 -0500

    set up backend and a little testing

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/users', (req, res) => {
-
+    const userData = [
+        {
+            "id": 1,
+            "username": "kyote",
+            "email": "kyote143@gmail.com"
+        },
+        {
+            "id": 2,
+            "username": "mingLee",
+            "email": "minglee@gmail.com"
+        }
+    ]
+    res.send(userData)
 })
 module.exports = router
