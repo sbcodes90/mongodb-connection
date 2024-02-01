70a4386e5b0db7403aa0dfd78481bbd75846f2f2
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-02-01 15:25:53 -0500

    added gradiant to background something fun

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -8,7 +8,8 @@ function App() {
   const [email, setEmail] = useState("");
 
   return (
-    <div className="bg-teal-400 h-screen px-[20px] lg:px-[150px] py-20">
+
+    <div className="bg-gradient-to-r from-teal-200 via-teal-500  to-teal-800 h-screen px-[20px] lg:px-[150px] py-20">
       <SignUpForm username={username} password={password} email={email} setUsername={setUsername} setPassword={setPassword} setEmail={setEmail}/>  
     </div>
   );
