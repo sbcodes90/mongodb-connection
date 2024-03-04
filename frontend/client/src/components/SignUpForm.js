e0483049629e349d8d008034c7262050549b9ab1
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-04 12:46:38 -0500

    loading screen done yay!

--- frontend/client/src/components/SignUpForm.js
+++ frontend/client/src/components/SignUpForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import StatusUpdatePopUp from "./StatusUpdatePopUp";
+import LoadingScreen from "./LoadingScreen";
 
 function SignUpForm({
   username,
@@ -106,7 +106,7 @@ function SignUpForm({
 </div>
 }
 { isLoading &&
-<StatusUpdatePopUp />
+<LoadingScreen />
 }
 </>
   );
