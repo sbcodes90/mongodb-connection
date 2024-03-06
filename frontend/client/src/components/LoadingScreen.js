4344a9a2660e487e9fb86fe52e132d7faccc557b
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-06 15:06:06 -0500

    add loading screen props

--- frontend/client/src/components/LoadingScreen.js
+++ frontend/client/src/components/LoadingScreen.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function LoadingScreen() {
+function LoadingScreen({message}) {
   return (
   <div className="">
-    <div className="bg-teal-500 text-white font-semibold text-4xl opacity-100 flex h-screen items-center justify-center">Saving
+    <div className="bg-teal-500 text-white font-semibold text-4xl opacity-100 flex h-screen items-center justify-center">{message}
     <div class="animate-spin inline-block size-12 border-[3px] border-current border-t-teal-600 text-teal-200 rounded-full dark:text-teal-500 ml-5 mt-2" role="status" aria-label="loading">
 </div>
 </div>
