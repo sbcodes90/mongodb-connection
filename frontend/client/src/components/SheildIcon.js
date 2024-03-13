c7199331691da9aab724d4487988a86cac15cf04
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-13 13:36:37 -0400

    install icons

---
+++ frontend/client/src/components/SheildIcon.js
@@ -0,0 +1,9 @@
+import { ShieldExclamationIcon } from '@heroicons/react/24/solid'
+
+export default function SheildIcon() {
+  return (
+
+    <ShieldExclamationIcon className="h-20 w-20 text-black  lg:ml-3" />
+
+  )
+}
