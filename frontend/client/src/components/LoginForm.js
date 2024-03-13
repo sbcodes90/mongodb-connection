4082c6ba3fd246127e200db6e3e750d2da94208c
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-12 20:07:02 -0400

    css and response timeout

--- frontend/client/src/components/LoginForm.js
+++ frontend/client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import LoadingScreen from './LoadingScreen';
 
-function LoginForm({ username, password, setUsername, setPassword}) {
+function LoginForm({ username, password, setUsername, setPassword }) {
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState(false);
   const navigate = useNavigate();
@@ -13,91 +13,86 @@ function LoginForm({ username, password, setUsername, setPassword}) {
     setIsLoading(true)
     axios
       .post("http://localhost:4000/login", { username, password })
-      .then(response => { 
+      .then(response => {
         //the response is the token
-        setIsLoading(false)
         console.log('response', response)
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 5000);
         return navigate(`/welcome/${username}`)
-        /* if (response.status === 200) {
-         
-        return navigate(`/welcome/${username}`)
-        }
-         if(!response) {
-         return navigate(`/`)
-        } */
-      
-      })   
+      })
       .catch(function (error) {
         if (error.response) {
-      
           console.log(error.response)
-          setIsLoading(false)
-          setUsername("")
-          setPassword("")
+          setTimeout(() => {
+            setIsLoading(false)
+          }, 5000);         
+          // setUsername("")
+          //setPassword("")
           setErrors(true)
         }
       });
   };
   return (
     <>
-    {!isLoading && 
-    <div className="bg-gradient-to-r from-teal-200 via-teal-500  to-teal-800 h-screen px-[20px] lg:px-[150px] py-20"> 
-    <div className="bg-white rounded-3xl lg:mt-[50px] mx-auto max-w-screen-sm px-4 py-16 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">Log In</h1>
-      </div>
+      {!isLoading &&
+        <div className="bg-gradient-to-r from-teal-200 via-teal-500  to-teal-800 h-screen px-[20px] lg:px-[150px] py-20">
+          <div className="bg-white rounded-3xl lg:mt-[50px] mx-auto max-w-screen-sm px-4 py-16 sm:px-6 lg:px-8">
+            <div className="mx-auto max-w-lg text-center">
+              <h1 className="text-2xl font-bold sm:text-3xl">Log In</h1>
+            </div>
 
-      <form action="post" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-        <div>
-          <label className="sr-only">
-            Username
-          </label>
+            <form action="post" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+              <div>
+                <label className="sr-only">
+                  Username
+                </label>
 
-          <div className="relative">
-            <input
-              type="email"
-              className={`w-full rounded-lg ${errors ? 'border-red-500': 'border-gray-200'} p-4 pe-12 text-md shadow-md border-4`}
-              placeholder="Enter username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            {errors && <p className="text-red-500 mt-2">Invalid Username</p>}
+                <div className="relative">
+                  <input
+                    type="email"
+                    className={`w-full rounded-lg ${errors ? 'border-red-500' : 'border-gray-200'} p-4 pe-12 text-md shadow-md border-3`}
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  {errors && <p className="text-red-500 mt-2">Invalid Username</p>}
+                </div>
+              </div>
+
+              <div>
+                <label className="sr-only">
+                  Password
+                </label>
+
+                <div className="relative">
+                  <input
+                    type="password"
+                    className={`w-full rounded-lg ${errors ? 'border-red-500' : 'border-gray-200'} p-4 pe-12 text-md shadow-md border-3`}
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  {errors && <p className="text-red-500 mt-2">Invalid Password</p>}
+                </div>
+              </div>
+              <div className="flex items-center justify-center pt-5">
+                <button
+                  type="submit"
+                  className="inline-block rounded-lg bg-teal-600 px-10 py-3 text-md font-medium text-white"
+                  onClick={handleSubmit}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-
-        <div>
-          <label className="sr-only">
-            Password
-          </label>
-
-          <div className="relative">
-            <input
-              type="password"
-              className={`w-full rounded-lg ${errors ? 'border-red-500': 'border-gray-200'} p-4 pe-12 text-md shadow-md border-4`}
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors && <p className="text-red-500 mt-2">Invalid Password</p>}
-          </div>
-        </div>
-        <div className="flex items-center justify-center pt-5">
-          <button
-            type="submit"
-            className="inline-block rounded-lg bg-teal-600 px-10 py-3 text-md font-medium text-white"
-            onClick={handleSubmit}
-          >
-            Login
-          </button>
-        </div>
-      </form>
-    </div>
-</div>
-}
-{ isLoading &&
-<LoadingScreen message="Logging In..."/>
-}
-</>
+      }
+      {isLoading &&
+        <LoadingScreen message="Logging In..." />
+      }
+    </>
   )
 }
 
