c906e022ee12795f5ba9af54f7f4b9251a378c93
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-15 14:19:14 -0400

    more css updates

--- frontend/client/src/components/NewWelcomePage.js
+++ frontend/client/src/components/NewWelcomePage.js
@@ -11,8 +11,8 @@ function NewWelcomePage() {
         <div className="flex flex-col justify-center items-center min-h-screen  ">
     <div className="text-5xl font-black pb-20 ">Admin Security App <LockClosedIcon className='w-10 h-10 inline-block mt-[-10px]' color='black' /></div>
     <div className='space-x-10'>
-    <Link className='border-white border-2  px-20 py-5 rounded-lg font-bold' to="/login">Login</Link>
-    <Link className='border-white border-2  px-20 py-5 rounded-lg font-bold'  to="/signup">Sign Up</Link>
+    <Link className='border-teal-200 border-4  px-20 py-5 rounded-lg font-bold text-xl' to="/login">Login</Link>
+    <Link className='border-teal-200 border-4  px-20 py-5 rounded-lg font-bold text-xl'  to="/signup">Sign Up</Link>
     </div>
     </div> 
             <li></li>
