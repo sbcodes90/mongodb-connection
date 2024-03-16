ec7e49d9715bf2a5f99ef27fc68d39b97631cb7b
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-15 20:35:50 -0400

    clear form and css updates

--- frontend/client/src/components/SignUpForm.js
+++ frontend/client/src/components/SignUpForm.js
@@ -22,6 +22,8 @@ function SignUpForm({
       setIsLoading(true)
       const response = await axios.post("http://localhost:4000/createUser", { username, password, email })
       console.log('response', response)
+        setUsername("")
+        setPassword("")
       setIsLoading(false)
       navigate(`/welcome/${username}`)
     } catch(err){
@@ -40,7 +42,7 @@ function SignUpForm({
     <>
     { !isLoading &&
     <div className="bg-gradient-to-r from-teal-200 via-teal-500  to-teal-800 h-screen px-[20px] lg:px-[150px] py-20"> 
-      <div className="bg-white rounded-3xl lg:mt-[190px] mx-auto max-w-screen-sm px-4 py-16 sm:px-6 lg:px-8">
+      <div className="bg-white rounded-3xl lg:mt-[90px] mx-auto max-w-screen-sm px-4 py-16 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-lg text-center">
           <h1 className="text-2xl font-bold sm:text-3xl">Sign Up</h1>
         </div>
