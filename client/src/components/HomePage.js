6d2df33a0d0cde26f8711bfff9ac75d0d7c02e25
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-20 20:42:56 -0400

    homepagecss

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -11,10 +11,10 @@ function HomePage() {
         <ul class="circles">
         <div className="flex flex-col justify-center items-center min-h-screen  ">
           <SecurityIcon />
-    <div className="text-5xl font-black pb-20 ">Admin Security App <LockClosedIcon className='w-10 h-10 inline-block mt-[-10px]' color='black' /></div>
-    <div className='space-x-10'>
-    <Link className='border-teal-200 border-4  px-20 py-5 rounded-lg font-bold text-xl' to="/login">Login</Link>
-    <Link className='border-teal-200 border-4  px-20 py-5 rounded-lg font-bold text-xl'  to="/signup">Sign Up</Link>
+    <div className="text-3xl lg:text-5xl font-black pb-20 ">Admin Security App <LockClosedIcon className='w-10 h-10 inline-block mt-[-10px]' color='black' /></div>
+    <div className=' space-y-10  lg:space-x-10 lg:space-y-0 flex flex-col justify-center items-center lg:flex-row'>
+    <Link className='border-teal-200 border-4  px-[65px] lg:px-20 py-5 rounded-lg font-bold text-xl' to="/login">Login</Link>
+    <Link className='border-teal-200 border-4  px-[60px] lg:px-20 py-5 rounded-lg font-bold text-xl'  to="/signup">Sign Up</Link>
     </div>
     </div> 
             <li></li>
