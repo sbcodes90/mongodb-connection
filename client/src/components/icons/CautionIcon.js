6aa1bc6782dfb94323f021591521b2ad2565360b
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-21 12:33:55 -0400

    icon css fix

--- client/src/components/icons/CautionIcon.js
+++ client/src/components/icons/CautionIcon.js
@@ -3,7 +3,7 @@ import { ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 export default function CautionIcon() {
   return (
 
-    <ExclamationTriangleIcon className="h-10 w-10 text-yellow-300  lg:ml-3" />
+    <ExclamationTriangleIcon className="h-5 w-5 lg:h-10 lg:w-10 text-yellow-300 ml-2 mt-1  lg:ml-3 lg:mt-0" />
 
   )
 }
