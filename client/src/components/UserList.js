3de2a886b777fc08c5badf80c873909d714f3135
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-21 16:32:43 -0400

    changed mobile view for table

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -10,23 +10,20 @@ function UserList() {
   const [loading, setLoading] = useState(true);
 
   const getData = async () => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(async () => {
       const response = await axios.get(`${baseUrl}/userlist`);
       setUserList(response.data);
-      setLoading(false)
-      return response
-    }, 1000)
-
-
+      setLoading(false);
+      return response;
+    }, 1000);
   };
 
   const deleteUser = async (id) => {
-    const response = await axios.delete(`${baseUrl}/userlist/${id}`)
+    const response = await axios.delete(`${baseUrl}/userlist/${id}`);
     getData();
     return console.log(response);
-
-  }
+  };
 
   const navigate = useNavigate();
 
@@ -36,12 +33,11 @@ function UserList() {
   };
 
   useEffect(() => {
-
-    const token = localStorage.getItem('auth-token');
+    const token = localStorage.getItem("auth-token");
 
     if (!token) {
-      alert('Sorry token expired please login again')
-      return navigate('/login')
+      alert("Sorry token expired please login again");
+      return navigate("/login");
     }
 
     getData();
@@ -56,71 +52,111 @@ function UserList() {
           <CautionIcon />
         </div>
 
-        {loading ? <Spinner message={'Loading.....'} /> : (
-          <div className="flex flex-col p-20">
-            <div className="-m-1.5 overflow-x-auto">
-              <div className="p-1.5 min-w-full inline-block align-middle">
-                <div className="border rounded-lg overflow-hidden">
-                  <table className="min-w-full divide-y divide-gray-500">
-                    <thead className="bg-gray-300">
-                      <tr>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-center text-xs font-medium text-black uppercase"
-                        >
-                          User
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-center text-xs font-medium text-black uppercase"
-                        >
-                          Email
-                        </th>
-                        <th
-                          scope="col"
-                          className="px-6 py-3 text-end text-xs font-medium text-black uppercase"
-                        >
-                          Action
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                      {userList.map((user) => {
-                        return (
-                          <tr key={user._id} className="bg-white">
-                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                              {user.username}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                              {user.email}
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                              <button
-                                onClick={() => deleteUser(user._id)}
-                                type="button"
-                                className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                              >
-                                Delete
-                              </button>
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </table>
-
+        {loading ? (
+          <Spinner message={"Loading....."} />
+        ) : (
+          <>
+            <div className="hidden md:flex lg:flex md:flex-col lg:flex-col p-20">
+              <div className="-m-1.5 overflow-x-auto">
+                <div className="p-1.5 min-w-full inline-block align-middle">
+                  <div className="border rounded-lg overflow-hidden">
+                    <table className="min-w-full divide-y divide-gray-500">
+                      <thead className="bg-gray-300">
+                        <tr>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-center text-xs font-medium text-black uppercase"
+                          >
+                            User
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-center text-xs font-medium text-black uppercase"
+                          >
+                            Email
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-end text-xs font-medium text-black uppercase"
+                          >
+                            Action
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                        {userList.map((user) => {
+                          return (
+                            <tr key={user._id} className="bg-white">
+                              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                                {user.username}
+                              </td>
+                              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                                {user.email}
+                              </td>
+                              <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                <button
+                                  onClick={() => deleteUser(user._id)}
+                                  type="button"
+                                  className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                >
+                                  Delete
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                  <button
+                    onClick={logout}
+                    className="border-white border-2 text-white px-20  py-5 rounded-lg font-bold text-2xl mt-20"
+                  >
+                    Logout
+                  </button>
                 </div>
-                <button
-                  onClick={logout}
-                  className="border-white border-2 text-white px-20  py-5 rounded-lg font-bold text-2xl mt-20"
-                >
-                  Logout
-                </button>
               </div>
             </div>
-          </div>)}
+            <div className="md:hidden lg:hidden p-20">
+              {userList.map((user) => {
+                return (
+                  <div
+                    key={user._id}
+                    className="bg-white rounded-3xl shadow-2xl py-10 px-5 mb-10"
+                  >
+                    <div
+                      id="text-container"
+                      className=" flex flex-col text-left "
+                    >
+                      <div className="font-black">
+                        Username:
+                        <span className="font-medium ml-2">
+                          {user.username}
+                        </span>
+                      </div>
+                      <div className="font-black">
+                        Email:
+                        <span className="font-medium ml-2">{user.email}</span>
+                      </div>
+                      
+                    </div>
+                    <button  onClick={() => deleteUser(user._id)}
+                                  type="button" className="bg-teal-900 text-white font-black flex justify-center px-10 py-3 rounded-3xl mt-10">
+                      Remove
+                    </button>
+                  </div>
+                );
+              })}
+               <button
+                    onClick={logout}
+                    className="border-white border-4 text-white px-20  py-3 rounded-lg font-bold text-2xl mt-5"
+                  >
+                    Logout
+                  </button>
+            </div>
+          </>
+        )}
       </div>
-
     </>
   );
 }
