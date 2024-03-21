0bdddf31450c84fda70b1a31e7f6a2d1f5369de4
sbcodes90 <sydneycburrell29@gmail.com>
2024-03-20 21:07:38 -0400

    welcome page css fix

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -20,37 +20,25 @@ function WelcomePage() {
 
   return (
     <>
-      <div
-        className={
-           `bg-teal-500 min-h-screen text-center`
-           
-        }
-      >
+        <div class="area">
+  
+  <ul class="circles">
+      <div className={` min-h-screen text-center`}>
         <div className="flex flex-col items-center pt-[200px] lg:inline-flex lg:mt-[100px]">
-          <div
-            className={
-              
-                "font-bold text-3xl text-white"
-                
-            }
-          >
-            {token ? (
-              <div>Welcome! {username}</div>
-            ) : (
-              "Please login"
-            )}
+          <div className={"font-bold text-3xl text-white"}>
+            {token ? <div>Welcome! {username}</div> : "Please login"}
           </div>
           {token && (
-            <div className="flex justify-center">
+            <div className="flex justify-center items-center flex-col lg:flex-row">
               <Link
-                to={ token ? "/userlist" : "/"}
-                className="bg-teal-300 text-black px-20 py-5 rounded-lg font-bold mt-20 mr-10"
+                to={token ? "/userlist" : "/"}
+                className="bg-teal-300 text-black px-20 py-5 rounded-lg font-bold mt-20 lg:mr-10"
               >
                 Admin
               </Link>
               <button
                 onClick={logout}
-                className="border-white border-2 text-white px-20  py-5 rounded-lg font-bold mt-20"
+                className="border-white border-2 text-white px-20 py-5 rounded-lg font-bold mt-5"
               >
                 Logout
               </button>
@@ -64,10 +52,22 @@ function WelcomePage() {
               className="py-[20px] px-20  mb-2 text-lg font-medium text-white focus:outline-none rounded-lg border-white border-2 focus:z-10 focus:ring-2 focus:ring-blue-400"
             >
               Login
-            </Link>   
+            </Link>
           </div>
         )}
       </div>
+      <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </div>
     </>
   );
 }
