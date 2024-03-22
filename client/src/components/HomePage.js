08557c2a72128fbc3bbc0b55384f73a773ba7a81
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-22 11:45:22 -0400

    css updates to mobile home screen

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { LockClosedIcon } from '@heroicons/react/24/solid'
 import SecurityIcon from './icons/SecurityIcon'
 
 function HomePage() {
@@ -10,8 +9,8 @@ function HomePage() {
   
         <ul class="circles">
         <div className="flex flex-col justify-center items-center min-h-screen  ">
-          <SecurityIcon />
-    <div className="text-3xl lg:text-5xl font-black pb-20 ">Admin Security App <LockClosedIcon className='w-10 h-10 inline-block mt-[-10px]' color='black' /></div>
+         <SecurityIcon className="w-5 h-5"  />
+    <div className="text-2xl lg:text-5xl font-black pb-10 lg:pb-20 ">Admin Security App </div>
     <div className=' space-y-10  lg:space-x-10 lg:space-y-0 flex flex-col justify-center items-center lg:flex-row'>
     <Link className='border-teal-200 border-4  px-[65px] lg:px-20 py-5 rounded-lg font-bold text-xl' to="/login">Login</Link>
     <Link className='border-teal-200 border-4  px-[60px] lg:px-20 py-5 rounded-lg font-bold text-xl'  to="/signup">Sign Up</Link>
