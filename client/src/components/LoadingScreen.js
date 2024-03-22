16f4df2b48ff8a2acbebf6f9ab8ddbd86acc8cfd
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-03-22 11:45:07 -0400

    loading screen text smaller mobile

--- client/src/components/LoadingScreen.js
+++ client/src/components/LoadingScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 function LoadingScreen({message}) {
   return (
   <div>
-    <div className="bg-teal-500 text-white font-semibold text-4xl opacity-100 flex h-screen items-center justify-center">{message}
+    <div className="bg-teal-500 text-white font-semibold text-2xl lg:text-5xl opacity-100 flex h-screen items-center justify-center">{message}
     <div class="animate-spin inline-block size-12 border-[3px] border-current border-t-teal-600 text-teal-200 rounded-full dark:text-teal-500 ml-5 mt-2" role="status" aria-label="loading">
 </div>
 </div>
